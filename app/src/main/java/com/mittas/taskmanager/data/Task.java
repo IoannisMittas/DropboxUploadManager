8ebd1b320de06a19638a81fb9c5ab48bc8287b9e
Yannis <mittas.ioannis@gmail.com>
2018-03-22 00:58:22 +0200

    building Task

--- app/src/main/java/com/mittas/taskmanager/data/Task.java
+++ app/src/main/java/com/mittas/taskmanager/data/Task.java
@@ -1,8 +1,53 @@
 package com.mittas.taskmanager.data;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
 /**
  * Created by John on 21-Mar-18.
  */
 
+@Entity
 public class Task {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    private String name;
+
+    private String description;
+
+    private String fileName;
+
+    private int time;
+
+    private String status;
+
+    public Task(String name, String description, String fileName) {
+        this.name = name;
+        this.description = description;
+        this.fileName = fileName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
--- app/src/main/java/com/mittas/taskmanager/ui/MainActivity.java
+++ app/src/main/java/com/mittas/taskmanager/ui/MainActivity.java
@@ -24,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
         fragmentsPagerAdapter = new FragmentsPagerAdapter(getSupportFragmentManager());
 
         viewPager = findViewById(R.id.view_pager);
