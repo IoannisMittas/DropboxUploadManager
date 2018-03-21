e568dc5407b62397f1e4fba7526c3d758b82b4eb
Yannis <mittas.ioannis@gmail.com>
2018-03-21 13:49:20 +0200

    added custom non scrollable viewpager

--- app/src/main/java/com/mittas/taskmanager/ui/MainActivity.java
+++ app/src/main/java/com/mittas/taskmanager/ui/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
