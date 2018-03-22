090230a7e6931fd7a94b645e96ff983492330581
Yannis <mittas.ioannis@gmail.com>
2018-03-22 06:32:07 +0200

    building toolbario

--- app/src/main/java/com/mittas/taskmanager/ui/AddTaskActivity.java
+++ app/src/main/java/com/mittas/taskmanager/ui/AddTaskActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,4 +65,24 @@ public class AddTaskActivity extends AppCompatActivity {
 
         return true;
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add_file) {
+           // TODO start filepicker
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
